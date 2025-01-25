8a0ec579ed28985da8df6050bfd397321b1219d8
WSommerfeld <marcinlasak5@gmail.com>
2025-01-25 17:29:54 +0100

    PVP

--- GUI.py
+++ GUI.py
@@ -1,4 +1,8 @@
 import tkinter as tk
+from tkinter import messagebox
+import gaming
+from gaming import PVPpozycje, updateguessed, PVPliczby, initpozycje, initliczby
+
 
 class Menu:
     def __init__(self):
@@ -27,21 +31,145 @@ class Menu:
         PVPGUI()
 
 class PVPGUI:
+
     def __init__(self):
         self.length=0
         self.length = rules().length
-        if self.length==0 or self.length=="":
+        if self.length==0 or self.length=="" or int(self.length)<=0:
             return
+
+        self.iterations=0
+        self.guessed=["_" for i in range(int(self.length))]
+        self.traf = "traf"
+        self.pozycje = gaming.initpozycje(int(self.length))
+        self.liczby = gaming.initliczby(int(self.length))
+        self.gaming = True
+        self.count = 0
 
         print(self.length)
         self.root = tk.Tk()
         self.root.geometry('500x500')
         self.root.resizable(False, False)
-        self.root.title('Odgadywanie')
-        label = tk.Label(self.root, text="Zgadywanie")
-        label.pack()
+
+        while(self.gaming):
+            self.gracz1()
+            self.gracz2()
+            count = 0
+            for i in range(int(self.length)):
+
+                if self.guessed[i] == "_":
+                    count += 1
+
+            if count == 0:
+                self.gaming = False
+
+        messagebox.showinfo("Wygrana!", "Brawo! Zgadłeś szyfr gracza nr 2.")
+        self.root.destroy()
+        Menu()
+
+    def gracz1(self):
+        self.root.title("Zgaduj!")
+
+        guessed_var = tk.StringVar()
+        guessed_var.set(str(self.guessed))
+        lista = " "
+        liczbyvar = tk.StringVar()
+        for i in range(int(self.length)+1):
+            if self.liczby[i] != 0:
+                lista = lista + str(i) + " wystąpiło " + str(self.liczby[i]) +" razy, "
+        liczbyvar.set(lista)
+        #print(lista)
+        if self.iterations>0:
+            tk.Label(self.root, textvariable=guessed_var,
+            font=("Arial", 24, "bold")).pack(pady=10)
+            tk.Label(self.root, textvariable=liczbyvar,wraplength=400,
+                     font=("Arial", 12, "bold")).pack(pady=10)
+
+        text_var = tk.StringVar()
+        text_var.set("Wpisz kod o długości "+str(self.length))
+        tk.Label(self.root,textvariable=text_var,
+        font=("Arial", 16, "bold")).pack(pady=10)
+
+        self.entry = tk.Entry(self.root, width=20, font=('Arial 16'))
+        self.entry.pack(pady=20)
+
+        self.set_button = tk.Button(self.root, font=('Arial 16'), text="Zatwierdź",
+                                    command=self.guess,padx=50,pady=50)
+        self.set_button.pack(pady=10)
 
         self.root.mainloop()
+
+    def guess(self):
+        self.traf = self.entry.get()
+
+        if self.traf==None or len(self.traf)<0 or len(self.traf)!=int(self.length):
+            messagebox.showerror("Długość szyfru", "Proszę podać szyfr o prawidłowej długości")
+            return
+        for widget in self.root.winfo_children():
+            widget.pack_forget()
+        self.root.quit()
+
+
+
+
+    def gracz2(self):
+        self.root.title("Sprawdź!")
+
+        self.pozycje=initpozycje(int(self.length))
+        self.liczby=initliczby(int(self.length))
+
+
+        text_var = tk.StringVar()
+        text_var.set(self.traf)
+        tk.Label(self.root,textvariable=text_var,
+        font=("Arial", 16, "bold")).pack(pady=10)
+
+
+        text_var1 = tk.StringVar()
+        text_var1.set("Na których pozycjach ")
+        tk.Label(self.root,textvariable=text_var1,
+        font=("Arial", 16, "bold")).pack(pady=5)
+
+        text_var2 = tk.StringVar()
+        text_var2.set("znajdują się prawidłowe cyfry?")
+        tk.Label(self.root, textvariable=text_var2,
+                 font=("Arial", 16, "bold")).pack(pady=10)
+
+
+
+        self.entryp = tk.Entry(self.root, width=20, font=('Arial 16'))
+        self.entryp.pack(pady=20)
+
+        text_var3 = tk.StringVar()
+        text_var3.set("Jakie cyfry wystąpiły")
+        tk.Label(self.root, textvariable=text_var3,
+                 font=("Arial", 16, "bold")).pack(pady=5)
+
+        text_var4 = tk.StringVar()
+        text_var4.set("na nieprawidłowych pozycjach?")
+        tk.Label(self.root, textvariable=text_var4,
+                 font=("Arial", 16, "bold")).pack(pady=10)
+
+        self.entryl = tk.Entry(self.root, width=20, font=('Arial 16'))
+        self.entryl.pack(pady=20)
+
+        self.setp_button = tk.Button(self.root, font=('Arial 16'), text="Zatwierdź",
+                                    command=self.setpozycje,padx=50,pady=50)
+        self.setp_button.pack(pady=10)
+
+        self.root.mainloop()
+
+    def setpozycje(self):
+        PVPpozycje(self.pozycje,self.entryp.get())
+        updateguessed(self.guessed,self.pozycje,self.traf)
+        PVPliczby(self.liczby,self.entryl.get())
+        self.iterations+=1
+
+        for widget in self.root.winfo_children():
+            widget.pack_forget()
+        self.root.quit()
+
+
 
 class computerGUI:
     def __init__(self):
@@ -74,5 +202,9 @@ class rules:
 
     def start(self):
         self.length =self.entry.get()
+
+        if self.length==0 or self.length=="" or int(self.length)<=0:
+            messagebox.showerror("Błąd podania długości", "Proszę podać prawidłową długość szyfru!")
+            return
         self.root.destroy()
         return self.length
--- main.py
+++ main.py
@@ -1,10 +1,25 @@
 import GUI
+import gaming
 def main():
-    print("Hello world")
+
 
     GUI.Menu()
+    '''
+    #test string-->pozycje
+    pozycje=gaming.initpozycje(5)
+    print(pozycje)
+    gaming.PVPpozycje(pozycje,"1,2")
+    print(pozycje)
+    '''
 
-
+    '''
+    #test string-->liczby
+    liczby = gaming.initliczby(5)
+    print(liczby)
+    gaming.PVPliczby(liczby,"1,1,1,1,3,1,1,1")
+    print(liczby)
+    '''
+    #gaming.cmdPVP(4)
 
 if __name__ == '__main__':
     main()
