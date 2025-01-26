96c54a10bbea70c4fc931a51d6dbb75829cf231b
FilipUrbanski05 <287355@student.pwr.edu.pl>
2025-01-26 10:59:47 +0100

    Update Zgadywanie.py

--- Zgadywanie.py
+++ Zgadywanie.py
@@ -1,1 +1,12 @@
-import random 
+import random
+
+def generuj_szyfr(n):
+return [random.randint(0,9) for _ in range(n)]
+
+def sprawdzam(code,guess):
+exact = sum(c == g for c, g in zip(code,guess))
+partial = sum(min(code.count(x), guess.count(x)) for x in set(guess)) - exact
+return exact,partial
+"""
+Praca w toku
+"""
