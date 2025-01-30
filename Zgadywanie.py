6d2b05f46e2b1122eccf8740471a7d8f0b3bf7f5
FilipUrbanski05 <287355@student.pwr.edu.pl>
2025-01-30 10:49:27 +0100

    komputer-gracz(gracz zgaduje).py

--- Zgadywanie.py
+++ Zgadywanie.py
@@ -5,22 +5,21 @@ def generate_code(n):
     return [random.randint(0, 9) for _ in range(n)]
 
 def evaluate_guess(code, guess):
-    """Ocena odgadnięcia - zwraca liczbę trafień na właściwych i niewłaściwych miejscach."""
+    """Ocena odgadniecia - zwraca liczbe trafien na wlaciwych i niewlasciwych miejscach."""
     correct_position = sum(c == g for c, g in zip(code, guess))
     correct_digit = sum(min(code.count(d), guess.count(d)) for d in set(guess)) - correct_position
     return correct_position, correct_digit
 
 def play_game(n):
-    """Główna funkcja gry."""
     code = generate_code(n)
     attempts = 0
 
-    print(f"Witaj w grze! Odgadnij {n}-cyfrowy szyfr.")
+    print(f"Witaj w grze - odgadnij {n}-cyfrowy szyfr.")
 
     while True:
         guess = input(f"Podaj {n} cyfr (bez spacji): ")
         if len(guess) != n or not guess.isdigit():
-            print("Błędne dane, wpisz dokładnie", n, "cyfr.")
+            print("Bledne dane - wprowadz dokladnie ", n, "cyfr.")
             continue
 
         guess = [int(digit) for digit in guess]
@@ -29,11 +28,11 @@ def play_game(n):
         correct_position, correct_digit = evaluate_guess(code, guess)
 
         if correct_position == n:
-            print(f"Gratulacje! Odgadłeś szyfr {code} w {attempts} próbach.")
+            print(f"Gratulacje - odgadles szyfr {code} w {attempts} probach.")
             break
         else:
-            print(f"Poprawnych cyfr na właściwych miejscach: {correct_position}")
-            print(f"Poprawnych cyfr na złych miejscach: {correct_digit}")
+            print(f"Poprawnych cyfr na wlasciwych miejscach: {correct_position}")
+            print(f"Poprawnych cyfr na zlych miejscach: {correct_digit}")
 
-"""Uruchomienie gry dla 4-cyfrowego szyfru"""
+"""Uruchomienie gry dla n=4-cyfrowego szyfru"""
 play_game(4)
