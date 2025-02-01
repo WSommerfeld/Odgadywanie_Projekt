3914da175a4ff7ed66424be9397fb5139116c444
WSommerfeld <marcinlasak5@gmail.com>
2025-02-01 18:41:10 +0100

    exetesting

--- main.py
+++ main.py
@@ -9,7 +9,6 @@ def main():
     python main.py dlugość_szyfru rodzaj_rozgrywki
     rodzaj_rozgrywki € {"pvp", "PVP"}
     '''
-
     code_length=-1
     type = "x"
     try:
@@ -23,27 +22,6 @@ def main():
     # rozgrywka w GUI
     GUI.Menu(code_length, type)
 
-    #gaming.cmdPlayerGuessing(4)
-
-    #gaming.autoguessing(5,"21371")
-
-    #rozgrywka w konsoli456
-    # gaming.cmdPVP(4)
-    '''
-    #test string-->pozycje
-    pozycje=gaming.initpozycje(5)
-    print(pozycje)
-    gaming.PVPpozycje(pozycje,"1,2")
-    print(pozycje)
-    '''
-
-    '''
-    #test string-->liczby
-    liczby = gaming.initliczby(5)
-    print(liczby)
-    gaming.PVPliczby(liczby,"1,1,1,1,3,1,1,1")
-    print(liczby)
-    '''
 
 
 if __name__ == '__main__':
