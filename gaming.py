9d7cf6c5080758b2f97b14fd9b9b68f6b7dbff2b
FilipUrbanski05 <287355@student.pwr.edu.pl>
2025-02-01 18:13:29 +0100

    Update gaming.py

--- gaming.py
+++ gaming.py
@@ -14,34 +14,34 @@ while True:
         print("///Błąd - Wprowadź poprawną wartość///")
 
 #--------------------------------------------------------#
-j=1
-print("///Wprowadz cyfry do szyfru",n,"-elementowego///")
-while(j<=n):
-    cyfra = int(input())
-    Gracz.append(cyfra)
-    j=j+1
-print(Gracz)
+def first_player(n):
+    j=1
+    print("///Wprowadz cyfry do szyfru",n,"-elementowego///")
+    while(j<=n):
+        cyfra = int(input())
+        Gracz.append(cyfra)
+        j=j+1
+    return(Gracz)
 #--------------------------------------------------------#
-# Na samym poczatku jest losowo generowana lista zlozona z n cyfr #
-Komputer=[random.randint(0,9) for _ in range(n)]
-#print(Komputer)
+
 #--------------------------------------------------------#
-trafione=[]
-# Jest wprowadzana pomocnicza n-elem. lista wypelniona 'X' #
-for i in range(n):
-    trafione.append('X')
-print(trafione)
-#--------------------------------------------------------#
-while(Komputer != Gracz):
-    i=0
-    while(i <= n-1):
-        if(trafione[i] == 'X'): # Jesli w liscie 'trafione' w danym miejscu i nadal jest 'X', generowana jest losowa cyfra w liscie 'Komputer' w tym samym miejscu i #
-            Komputer[i]=random.randint(0,9)
-            #print("Komputer zgaduje:",Komputer)
+def guess_game(n,Gracz):
+    # Na samym poczatku jest losowo generowana lista zlozona z n cyfr #
+    Komputer=[random.randint(0,9) for _ in range(n)]
+    trafione=[]
+    # Jest wprowadzana pomocnicza n-elem. lista 'trafione' wypelniona 'X' #
+    for i in range(n):
+        trafione.append('X')
+    while(Komputer != Gracz):
+        i=0
+        while(i <= n-1):
+            if(trafione[i] == 'X'): # Jesli w liscie 'trafione' w danym miejscu i nadal jest 'X', generowana jest losowa cyfra w liscie 'Komputer' w tym samym miejscu i #
+                Komputer[i]=random.randint(0,9)
+                #print("Komputer zgaduje:",Komputer)
             # Na biezaco sprawdzane jest czy mamy juz trafiona cyfre, jezeli tak to zamieniamy 'X' na ta cyfre #
-            if(Komputer[i] == Gracz[i]):
-                print("Komputer zgadl liczbe:",Gracz[i],"na pozycji:",i)
-                trafione[i]=Komputer[i]
-                print(trafione)
-        i=i+1
-print("Gratulacje komputerze - udalo ci sie odgadnac szyfr")
+                if(Komputer[i] == Gracz[i]):
+                    print("Komputer zgadl liczbe:",Gracz[i],"na pozycji:",i)
+                    trafione[i]=Komputer[i]
+                    print(trafione)
+                    i=i+1
+    print("Gratulacje komputerze - udalo ci sie odgadnac szyfr")
